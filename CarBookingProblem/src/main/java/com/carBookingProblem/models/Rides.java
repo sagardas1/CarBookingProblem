c1cab8519ff5bbd6f0816c138c5f6ba29ea0b134
sagardas1 <sagardas.195@gmail.com>
2020-09-23 19:32:26 +0530

    changes

--- CarBookingProblem/src/main/java/com/carBookingProblem/models/Rides.java
+++ CarBookingProblem/src/main/java/com/carBookingProblem/models/Rides.java
@@ -13,30 +13,37 @@ public class Rides {
 	private String driverEmail;
 	private String userEmail;
 	private long date;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getDriverEmail() {
 		return driverEmail;
 	}
+
 	public void setDriverEmail(String driverEmail) {
 		this.driverEmail = driverEmail;
 	}
+
 	public String getUserEmail() {
 		return userEmail;
 	}
+
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
 	}
+
 	public long getDate() {
 		return date;
 	}
+
 	public void setDate(long date) {
 		this.date = date;
 	}
-	
 
 }
