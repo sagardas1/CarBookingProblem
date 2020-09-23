41a3d5b5aafd1379f901442738d7fdccd608708b
sagardas1 <sagardas.195@gmail.com>
2020-09-23 19:31:22 +0530

    changess

--- CarBookingProblem/src/main/java/com/carBookingProblem/models/LocationDetails.java
+++ CarBookingProblem/src/main/java/com/carBookingProblem/models/LocationDetails.java
@@ -8,36 +8,42 @@ import javax.persistence.Id;
 @Entity
 public class LocationDetails {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long locationId;
 	private double lat;
 	private double lon;
 	private String email;
+
 	public Long getLocationId() {
 		return locationId;
 	}
+
 	public void setLocationId(Long locationId) {
 		this.locationId = locationId;
 	}
-	
+
 	public double getLat() {
 		return lat;
 	}
+
 	public void setLat(double lat) {
 		this.lat = lat;
 	}
+
 	public double getLon() {
 		return lon;
 	}
+
 	public void setLon(double lon) {
 		this.lon = lon;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
 
 }
