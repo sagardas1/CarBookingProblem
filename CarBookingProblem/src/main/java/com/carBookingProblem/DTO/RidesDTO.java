8e9061c8593787e5f1801279174c2e27c463bbb7
sagardas1 <sagardas.195@gmail.com>
2020-09-23 19:31:06 +0530

    chnages

--- CarBookingProblem/src/main/java/com/carBookingProblem/DTO/RidesDTO.java
+++ CarBookingProblem/src/main/java/com/carBookingProblem/DTO/RidesDTO.java
@@ -4,23 +4,29 @@ public class RidesDTO {
 	private String driverEmail;
 	private String userEmail;
 	private long date;
+
 	public String getDriverEmail() {
 		return driverEmail;
 	}
+
 	public void setDriverEmail(String driverEmail) {
 		this.driverEmail = driverEmail;
 	}
+
 	public String getUserEmail() {
 		return userEmail;
 	}
+
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
 	}
+
 	public long getDate() {
 		return date;
 	}
+
 	public void setDate(long date) {
 		this.date = date;
 	}
-	
+
 }
