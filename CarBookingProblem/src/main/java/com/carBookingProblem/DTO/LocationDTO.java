363a88ccda64f47e50aa98ed31148585528f4c4d
sagardas1 <sagardas.195@gmail.com>
2020-09-23 19:30:08 +0530

    changes

--- CarBookingProblem/src/main/java/com/carBookingProblem/DTO/LocationDTO.java
+++ CarBookingProblem/src/main/java/com/carBookingProblem/DTO/LocationDTO.java
@@ -5,23 +5,29 @@ public class LocationDTO {
 	private double x;
 	private double y;
 	private String email;
+
 	public double getX() {
 		return x;
 	}
+
 	public void setX(double x) {
 		this.x = x;
 	}
+
 	public double getY() {
 		return y;
 	}
+
 	public void setY(double y) {
 		this.y = y;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 }
