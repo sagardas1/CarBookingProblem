14ea824d8267c6e62659b8e5f632c3582c248a84
sagardas1 <sagardas.195@gmail.com>
2020-10-02 22:42:39 +0530

    CHANGES

--- CarBookingProblem/src/main/java/com/carBookingProblem/constants/Constants.java
+++ CarBookingProblem/src/main/java/com/carBookingProblem/constants/Constants.java
@@ -1,11 +1,8 @@
 package com.carBookingProblem.constants;
 
 public class Constants {
-	
-	
-	public static final int USERTYPE_COSTUMERS=1;
-	public static final int USERTYPE_DRIVERS=2;
-	
-	
+
+	public static final int USERTYPE_COSTUMERS = 1;
+	public static final int USERTYPE_DRIVERS = 2;
 
 }
