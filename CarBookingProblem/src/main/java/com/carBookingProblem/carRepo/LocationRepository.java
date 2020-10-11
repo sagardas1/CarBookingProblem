4d16b87138681e7b6d3e46fff6e19e1e3e77e969
sagardas1 <sagardas.195@gmail.com>
2020-10-11 18:03:20 +0530

    changes

--- CarBookingProblem/src/main/java/com/carBookingProblem/carRepo/LocationRepository.java
+++ CarBookingProblem/src/main/java/com/carBookingProblem/carRepo/LocationRepository.java
@@ -16,7 +16,6 @@ public interface LocationRepository extends CrudRepository<LocationDetails, Long
 	@Query(value = "UPDATE LocationDetails set x= :x,y=:y where email = :email", nativeQuery = true)
 	int updateLocation(double x, String email, double y);
 
-	
 	@Transactional
 	@Query(value = "select * from LocationDetails where email=:email", nativeQuery = true)
 	LocationDetails getDirverLocation(String email);
